8ff70aee460836c22bd5d3576d3399fc3e3f1c67
id0ntc4re <parsikovevgenij470@gmail.com>
2025-11-29 01:09:35 +0700

    qwe

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -63,7 +63,7 @@ export default function Header() {
             height={100}
           />
           <div className="name">
-            <span>Tereaiqos</span>
+            <span>qwe</span>
           </div>
         </Link>
         <div className="menu">

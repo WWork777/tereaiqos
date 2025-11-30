dcfebd55a37b2ac9d0325ad6688a0564e4d3a0f3
id0ntc4re <parsikovevgenij470@gmail.com>
2025-12-01 06:16:53 +0700

    qwe

--- src/app/checkout/page.jsx
+++ src/app/checkout/page.jsx
@@ -876,10 +876,7 @@ ${formattedCart}
                 </p>
               )}
             </div>
-            <button
-              onClick={handleExternalSubmit}
-              disabled={loading || selectedMethod === "pickup"}
-            >
+            <button onClick={handleExternalSubmit} disabled={loading}>
               {loading ? "Загрузка..." : "Заказать"}
             </button>
           </div>

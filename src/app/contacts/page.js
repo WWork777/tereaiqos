045d8eff7b375ce6a18b8c5967553016a5376211
Ser0jka123 <sersur42@yandex.ru>
2025-12-03 12:49:53 +0700

    upd

--- src/app/contacts/page.js
+++ src/app/contacts/page.js
@@ -40,7 +40,7 @@ const Contacts = () => {
         <h3>Социальные сети</h3>
         <p>
           Telegram:{" "}
-          <Link href="https://t.me/+tXZ1x8yraKUzNWEy">@TereaIqos</Link>
+          <Link href="https://t.me/Ilumastore2025">@TereaIqos</Link>
         </p>
         <p>
           Whatsapp:{" "}
